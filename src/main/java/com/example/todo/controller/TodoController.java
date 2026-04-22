af1b62dfe828ea2370a2a612687dca2c0ab0340e
abetakamasa <202601_aw_abe@x-ide.sakura.ne.jp>
2026-04-22 12:15:33 +0900

    Section 7-1 削除処理の実装

--- src/main/java/com/example/todo/controller/TodoController.java
+++ src/main/java/com/example/todo/controller/TodoController.java
@@ -7,6 +7,7 @@ import com.example.todo.service.TodoService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -51,6 +52,12 @@ public class TodoController {
         todo.setTitle(title);
         todo.setCompleted(false);
         todoService.create(todo);
+        return "redirect:/todo";
+    }
+
+    @PostMapping("/{id}/delete")
+    public String delete(@PathVariable("id") Long id) {
+        todoService.deleteById(id);
         return "redirect:/todo";
     }
 }
