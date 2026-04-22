77ead984ba75bb2430a36a7026121f2b3850d14e
abetakamasa <202601_aw_abe@x-ide.sakura.ne.jp>
2026-04-22 12:04:14 +0900

    Section 5-2 Mapperインターフェースの作成

---
+++ src/main/java/com/example/todo/mapper/TodoMapper.java
@@ -0,0 +1,12 @@
+package com.example.todo.mapper;
+
+import java.util.List;
+
+import com.example.todo.model.Todo;
+
+public interface TodoMapper {
+
+    List<Todo> findAll();
+
+    int insert(Todo todo);
+}
